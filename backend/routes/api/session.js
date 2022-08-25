a029a5bbb66dd37ec743f2c0f508bbfe37b7a07f
christopher <cpannella3@gmail.com>
2022-08-25 17:06:10 -0600

    get-session functionality added

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -35,4 +35,19 @@ router.post('/', async (req, res, next) => {
   );
 
 
+  // Restore session user
+router.get(
+  '/',
+  restoreUser,
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject()
+      });
+    } else return res.json({});
+  }
+);
+
+
 module.exports = router;
