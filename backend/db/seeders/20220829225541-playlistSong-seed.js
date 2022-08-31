1dcc62ae93ee215e7fc4bfdcae4d31aec0ad1871
christopher <cpannella3@gmail.com>
2022-08-31 13:34:56 -0600

    seed finally?

--- backend/db/seeders/20220829225541-playlistSong-seed.js
+++ backend/db/seeders/20220829225541-playlistSong-seed.js
@@ -6,49 +6,46 @@ const {Playlist, Song, PlaylistSong} = require("../models");
 
 
 
-const playlistSongs= [
 
-  {
-    name: 'Pink Floyd Favs',
-      song: {
-        name: 'Money'
-      }
-  },
-  {
-    name: 'Metal playlist',
-      song: {
-        name: 'The Pot'
-      }
-  },
-  {
-    name: 'Metal playlist',
-      song: {
-        name: 'Jambi'
-      }
-  },
+
+
+const inserts = [
+{
+  songId: 1,
+  playlistId: 1,
+  order: 1
+},
+{
+  songId: 2,
+  playlistId: 1,
+  order: 2
+},
+{
+  songId: 3,
+  playlistId: 1,
+  order: 3
+},
+{
+  songId: 4,
+  playlistId: 2,
+  order: 1
+},
+{
+  songId: 5,
+  playlistId: 2,
+  order: 2,
+},
+{
+  songId: 3,
+  playlistId: 1,
+  order: 3
+}
 ]
-
-
-
-
 
 module.exports = {
   async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('PlaylistSongs', inserts)
 
-    for (let playlistData of playlistSongs) {
-      const { name, song } = playlistData
-      const foundPlaylistName = await Playlist.findOne({
-        where: {name}
-      });
-      const foundSong = await Song.findOne({
-        where: {title: song.name}
-      })
-
-      await PlaylistSong.create({
-        playlistId: foundPlaylistName.id,
-        songId : foundSong.id
-      })
-     }
   },
 
   async down (queryInterface, Sequelize) {
