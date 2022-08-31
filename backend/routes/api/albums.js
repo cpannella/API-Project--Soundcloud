3f0e94ee18e9cc550c112376c2453fe349c3a7a9
christopher <cpannella3@gmail.com>
2022-08-31 17:14:19 -0600

    album create

--- backend/routes/api/albums.js
+++ backend/routes/api/albums.js
@@ -3,6 +3,16 @@ const router = express.Router()
 const {User, Song, Comment, Album} = require('../../db/models')
 const { requireAuth, restoreSession, restoreUser } = require('../../utils/auth');
 
+router.post('/', requireAuth, async (req, res) =>{
+  const {title, description, imageUrl} = req.body
+  const newAlbum = await Album.create({
+    title: title,
+    description: description,
+    imageUrl: imageUrl
+  })
+
+  res.json(newAlbum)
+})
 
 router.get('/', async (req, res) =>{
   const albums = await Album.findAll()
