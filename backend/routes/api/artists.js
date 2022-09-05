2ee81bc95bb4be130a2c70420c4d6a77a6fb881c
christopher <cpannella3@gmail.com>
2022-09-04 19:27:27 -0600

    commit issues

--- backend/routes/api/artists.js
+++ backend/routes/api/artists.js
@@ -6,6 +6,13 @@ const { requireAuth, restoreSession, restoreUser } = require('../../utils/auth')
 
 router.get('/:userId/playlists', requireAuth, async (req, res) => {
   const userId = req.user.id
+  if(!userId){
+    res.status(404)
+    res.json({
+      "message": "Artist couldn't be found",
+      "statusCode": 404
+    })
+  }
   const playlists = await Playlist.findAll({
     where: {
       userId : userId
