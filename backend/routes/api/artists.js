3c34b060e85b6cd7694695ce81fdcd72e0d96b86
christopher <cpannella3@gmail.com>
2022-09-04 19:39:32 -0600

    dayAfterWeddingCodingSession

--- backend/routes/api/artists.js
+++ backend/routes/api/artists.js
@@ -4,7 +4,7 @@ const router = express.Router()
 const {User, Song, Comment, Album, sequelize, Playlist} = require('../../db/models')
 const { requireAuth, restoreSession, restoreUser } = require('../../utils/auth');
 
-router.get('/:userId/playlists', requireAuth, async (req, res) => {
+router.get('/:userId/playlists/songs', requireAuth, async (req, res) => {
   const userId = req.user.id
   if(!await User.findByPk(userId)){
     res.status(404)
